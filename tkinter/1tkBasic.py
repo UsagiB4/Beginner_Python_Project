d52aa1bb57016b2fc2b43f68dffb064c790a06b4
UsagiB4 <binaryBunny@tutanota.com>
2021-09-16 19:47:46 +0600

    learning TKinter

---
+++ tkinter/1tkBasic.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()  # root widget
+
+basicLabel = Label(root, text="hello world!")  # Label Widget
+
+basicLabel.pack()  # packing everything to show on the screen.
+
+root.mainloop()
