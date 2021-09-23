1dac60d9cad56d6bd5612d8b9ecd2183df2711be
UsagiB4 <binaryBunny@tutanota.com>
2021-09-23 18:58:13 +0600

    Basic about  Window

--- tkinter/1tkBasic.py
+++ tkinter/1tkBasic.py
@@ -1,9 +1,22 @@
 from tkinter import *
 
-root = Tk()  # root widget
+# difference between window and widgets
+# window works as a container for widgets. It is an certain area that contains the widgets.
+#__________________________________________________
+# widgets are GUI elements. Such as, Button, textbox, labels, images and so on.
 
-basicLabel = Label(root, text="hello world!")  # Label Widget
 
-basicLabel.pack()  # packing everything to show on the screen.
+window = Tk()  # creating an window
 
-root.mainloop()
+window.geometry("560x360")  # it will give window the height and the width. _.geometry("WxH")
+window.title("My window")  # set a title of the window.
+
+# Setting an icon for the window.
+# tkinter can not use regular image as icon. so you have to convert it into a usable format.
+icon = PhotoImage(file='icon.png')  # this will convert our image.
+window.iconphoto(True, icon)  # Setting the icon to window.
+
+# changing the color of the window.
+window.config(backgroun="cyan")  # changing the window color into cyan
+
+window.mainloop()  # This will display the window.
