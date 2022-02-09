014df813587dc1f1570099da077c2ae90a4cda55
UsagiB4 <binaryBunny@tutanota.com>
2022-02-09 19:18:32 +0600

    Simple Clock

--- clock.py
+++ clock.py
@@ -5,12 +5,14 @@ from time import strftime
 root = Tk()
 root.title("Clock")
 
+
 def time():
-    string=strftime('%H:%M:%S %p')
+    string = strftime('%H:%M:%S %p')
     lable.config(text=string)
     lable.after(1000, time)
 
-lable = Label(root, font=("connection serif", 80), background ="black", foreground="cyan")
+
+lable = Label(root, font=("connection serif", 80), background="black", foreground="cyan")
 lable.pack(anchor="center")
 time()
 
